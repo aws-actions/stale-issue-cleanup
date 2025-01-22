33662e4bd9a89926fc0bd0f72807ecd61e12ab84
Alisha Kulkarni <alikulka@amazon.com>
2025-01-22 12:00:16 -0800

    feat: added a line to entrypoint.ts

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,3 +1,4 @@
+// Test comment in entrypoint.ts
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import { closeIssue, getIssues, getTimelineEvents, hasEnoughUpvotes, markStale, removeLabel } from './github';
